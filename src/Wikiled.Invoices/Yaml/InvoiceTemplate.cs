c2a6bce04b4c0504ae0f316fa0021e6087899795
Andrius Mudinas <keistokas@gmail.com>
2019-07-03 23:29:20 +0100

    Update

---
+++ src/Wikiled.Invoices/Yaml/InvoiceTemplate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wikiled.Invoices.Yaml
+{
+    public class InvoiceTemplate
+    {
+        //'remove_whitespace': False,
+        //'remove_accents': False,
+        //'lowercase': False,
+        //'currency': 'EUR',
+        //'date_formats': [],
+        //'languages': [],
+        //'decimal_separator': '.',
+        //'replace': []
+    }
+}
--- src/Wikiled.Invoices/Yaml/YamlLoader.cs
+++ src/Wikiled.Invoices/Yaml/YamlLoader.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using Wikiled.Common.Resources;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
 
 namespace Wikiled.Invoices.Yaml
 {
@@ -7,8 +12,24 @@ namespace Wikiled.Invoices.Yaml
     {
         public IEnumerable<YamlDefinition> Load()
         {
-            GetType().Assembly.GetManifestResourceNames();
-            throw new NotImplementedException();
+            var assembly = GetType().Assembly;
+            var files = assembly.GetManifestResourceNames();
+            foreach (var file in files)
+            {
+                using (var stream = assembly.GetEmbeddedFile(file))
+                {
+                    using (var reader = new StreamReader(stream))
+                    {
+                        var deserializer = new DeserializerBuilder()
+                                           .WithNamingConvention(new CamelCaseNamingConvention())
+                                           .Build();
+                        var yaml = new YamlStream();
+                        yaml.Load(reader);
+                        var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+                        mapping.
+                    }
+                }
+            }
         }
     }
 }
